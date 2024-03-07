a050cd60e88f1bdaa17a1b0c23c89fc7a577d499
FELIPE HONORIO MORAES <felipehonorio55@gmail.com>
2024-03-07 10:40:02 -0300

    Add files via upload

---
+++ functions-binary-tree.hpp
@@ -0,0 +1,4 @@
+#ifndef _HPP_PILHA_DINAMICA_CHAR_
+#define _HPP_PILHA_DINAMICA_CHAR_
+
+#include <iomanip>
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "functions-binary-tree.hpp"
+
+int main()
+{
+    // 0 – Sair
+    // 1 – Incluir
+    // 2 – Mostrar pré-ordem
+    // 3 – Mostrar ordem simétrica
+    // 4 – Mostrar pós-ordem
+
+    setlocale(LC_ALL, "Portuguese");
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
